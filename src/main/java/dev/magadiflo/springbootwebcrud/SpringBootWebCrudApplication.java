8c85ce4d054846465c6211fe5b4c86ef00d5f9cc
Martín <magadiflo@gmail.com>
2024-02-18 23:23:40 -0500

    Inicio

---
+++ src/main/java/dev/magadiflo/springbootwebcrud/SpringBootWebCrudApplication.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.springbootwebcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebCrudApplication.class, args);
+	}
+
+}
